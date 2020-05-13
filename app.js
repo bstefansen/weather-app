a743fda1543725a53b75211348cebce67eb59d3e
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-05-13 17:44:48 -0400

    revamped app.js

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 var url = "https://fcc-weather-api.glitch.me/api/current?";
-var urlTest = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
-var newURL;
 
-var getLocation = new Promise((resolve) => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      let lon = position.coords.longitude;
-      let lat = position.coords.latitude;
-      document.getElementById('location').innerHTML = "latitude: " + lat + "<br>longitude: " + lon;
-    });
-  }
-
-  resolve();
-  return { lon, lat };
-})
-newURL = url + 'lat=' + getLocation.lat + '&lon=' + getLocation.lon;
+var getLocation = () => {
+  return new Promise(resolve => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        let lon = position.coords.longitude;
+        let lat = position.coords.latitude;
+        document.getElementById('location').innerHTML = "latitude: " + lat + "<br>longitude: " + lon;
+        resolve(url + 'lat=' + lat + '&lon=' + lon);
+      });
+    } else {
+      resolve();
+    }
+  });
+}
 
 // newURL is undefined but urlTest works
-var getWeather = 
-  fetch(urlTest)
-    .then(res => res.json())
-    .then(data => JSON.stringify(data))
+var getWeather = async(url) => {
+  const res = await fetch(url);
+  const json = await res.json();
+  return json;
+}
     
 async function App() {
-  await getLocation;
-  const weather = await getWeather;
-  document.getElementById('test').innerHTML = newURL
-  document.getElementById('weather').innerHTML = weather
+  const location = await getLocation();
+  const weather = await getWeather(location);
+
+  var weatherData = [
+    weather.name, weather.main.temp, weather.weather[0].description
+  ]
+
+  weatherData[1] = weatherData[1] + "&degC"
+  document.getElementById('URL').innerHTML = location
+  document.getElementById('weather').innerHTML = weatherData.join(', ')
 }
 
 App()
